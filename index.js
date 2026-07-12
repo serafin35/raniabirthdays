f68dbfc9eb245eb2c3941e67dc36c4d6a99dae4d
Supranata Hadiwijaya <50736813+serafin35@users.noreply.github.com>
2026-07-12 09:57:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const birthday = new Date('2026-07-15T00:00:00');
-const ssap = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08';
+const ssap = 'a7cf42114ca93dcb83ab3125a50df02e9113c1c910fb66617952e3fb6577fec1';
 
 let state = 'COUNTING'; // COUNTING, GATE, REVEAL
 
@@ -92,23 +92,13 @@ function typeWriter(){
 }
 
 const bgMusic = new Audio('images/BGM.mp3');
-const inMusic = new Audio('images/introMusic.mp3');
+/* const inMusic = new Audio('images/introMusic.mp3'); */
 
 function tryPlay(){
-    if (state === 'COUNTING') {
-        inMusic.play().catch(() => {
-            document.removeEventListener('click', tryPlay);
-        document.removeEventListener('keydown', tryPlay);
-        bgMusic.pause();
-        });
-    } else {
-        bgMusic.play().catch(() => {
-            document.removeEventListener('click', tryPlay);
-        document.removeEventListener('keydown', tryPlay);
-        inMusic.pause();
-        });
-    }
-}
+    bgMusic.play().catch(() => {});
+    document.removeEventListener('click', tryPlay);
+     document.removeEventListener('keydown', tryPlay);
+};
 
 document.addEventListener('click', tryPlay);
 document.addEventListener('keydown', tryPlay);
